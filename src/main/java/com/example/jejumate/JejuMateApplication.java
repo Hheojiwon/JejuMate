3fbb8c38ed646b7996a837d031a5ed82e3eb39b2
hheojiwon <0618amy@naver.com>
2025-09-19 17:26:02 +0900

    init: 개발 초기 설정

---
+++ src/main/java/com/example/jejumate/JejuMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.jejumate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuMateApplication.class, args);
+	}
+
+}
